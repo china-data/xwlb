4d3d4dd0240ba38287a5971ec2a94fa99b7e9224
zhoukekestar <zhoukekestar@163.com>
2021-03-28 16:54:17 +0800

    feat: 2013 & 2014

--- scripts/init.js
+++ scripts/init.js
@@ -2,10 +2,18 @@ const loadData = require('../src/loadData');
 const moment = require('moment');
 
 // 最早的 20120702
-const START = new Date('2012-07-02').getTime();
-const END = new Date('2012-12-31').getTime();
+const START = new Date('2013-08-27').getTime();
+const END = new Date('2014-12-31').getTime();
 
-const ONE_DAY = 1000 * 60 * 60 * 24;
-for (let date = START; date <= END; date += ONE_DAY) {
-  loadData(moment(date).format('YYYYMMDD'));
-}
+!(async () => {
+
+  // 为防止给服务器太大压力，一次次请求
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+  for (let date = START; date <= END; date += ONE_DAY) {
+
+    const currentDate = moment(date).format('YYYYMMDD');
+    console.log(`[${currentDate}] loading...`);
+    loadData(currentDate);
+    console.log(`[${currentDate}] finished!`);
+  }
+})();
